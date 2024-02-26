8217dd8ba2a341f7020648620a332387824d584e
Murilo-MRS <murilosantana1993@gmail.com>
2024-02-26 15:43:07 -0300

    chore: add config vitest e tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
-import { fileURLToPath, URL } from 'node:url';
-
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [
-    vue()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+  plugins: [vue()],
+  test: {
+    // ...
   }
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,10 @@
-import { fileURLToPath } from 'node:url';
-import { mergeConfig, defineConfig } from 'vite';
-import { configDefaults } from 'vitest/config';
-import viteConfig from './vite.config';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/*'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-      transformMode: {
-        web: [/\.[jt]sx$/]
-      }
-    }
-  })
-);
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  test:{
+    globals:true
+  }
+});
